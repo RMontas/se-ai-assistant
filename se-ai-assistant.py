9599fd18da32446af06ef5892ca9a90ea09ca038
rmonteiro <rmonteiro@nvidia.com>
2021-10-15 21:58:34 +0200

    got specific line of csv. Need to correct csv because some numbers are wrong

--- se-ai-assistant.py
+++ se-ai-assistant.py
@@ -1,7 +1,16 @@
 import numpy as np
+import csv
 
 def main():
-    buildingsInput = "build.txt"
+    build_input = "build.csv"
+    build_cost = np.zeros((1,5))
+    build_prod = np.zeros((1,6))
+    with open(build_input, newline='') as csvfile:
+        table = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in table:
+            if row[0] == "Mina de Metal":
+                print(row)
+                
 
 
 if __name__ == "__main__":
